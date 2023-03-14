c648e77808b50bc435d75c00af6ac25ce647267c
66hakeem <66hakeem@gmail.com>
2023-03-14 16:14:05 +0300

    Adds CardView to Home Page

--- money-transfer/Views/ContentView.swift
+++ money-transfer/Views/ContentView.swift
@@ -9,8 +9,24 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            ZStack {
+                Color(#colorLiteral(red: 0.9645929933, green: 0.9640330672, blue: 1, alpha: 1)).edgesIgnoringSafeArea(.all)
+                ScrollView(.vertical) {
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        LazyHStack {
+                            ForEach(0..<10, id: \.self) { item in
+                                CardView(isSelected: item == 0)
+                                    .shadow(color: Color.black.opacity(0.2), radius: 5, x: 5, y: 5)
+                            }
+                        }
+                        .padding(20)
+                    }
+                }
+                //.background(Color(#colorLiteral(red: 0.9645929933, green: 0.9640330672, blue: 1, alpha: 1)))
+            }
+            .navigationTitle(Text("My Card"))
+        }
     }
 }
 
