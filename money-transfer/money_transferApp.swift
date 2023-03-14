a50e47c6698b7e16292b89c2dd84ce4d8002c0e7
66hakeem <66hakeem@gmail.com>
2023-03-14 12:27:34 +0300

    Initial Commit

---
+++ money-transfer/money_transferApp.swift
@@ -0,0 +1,17 @@
+//
+//  money_transferApp.swift
+//  money-transfer
+//
+//  Created by yinyang on 14/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct money_transferApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
